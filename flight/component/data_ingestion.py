79c719809496a57621a27d6f5e1f0095ed114ce7
sreeman <sreemanbitsmech@gmail.com>
2022-07-26 14:35:29 +0530

    data_ingestion.py

--- flight/component/data_ingestion.py
+++ flight/component/data_ingestion.py
@@ -45,7 +45,6 @@ class DataIngestion:
             # extract data into the file path
             logging.info(f"Downloading file from :[{download_url}] into "
                          ":[{tgz_file_path}]")
-            
             request.urlretrieve(download_url, tgz_file_path)
             logging.info(f"File :[{tgz_file_path}] has been downloaded "
                          "successfully.")
@@ -75,53 +74,86 @@ class DataIngestion:
             logging.info(f"Extraction completed")
             
         except Exception as e:
-            raise HousingException(e,sys) from e
+            raise FlightException(e,sys) from e
+    
+    def drop_rows_with_exception(X : pd.DataFrame, tol = 1, columns = ["Airline","Total_Stops"],)->None:
+        try:
+            drop_exp = False
+            for col in columns:
+                col_count = X[col].value_counts()
+                exception_cat = col_count[col_count == tol].index.values[0]
+                X.drop(X[X[col] == exception_cat].index,axis=0,inplace=True,)
+                X.reset_index(drop=True,inplace=True)
+                drop_exp = True
+                logging.info(f"Column <{col}> : Has category '{exception_cat}' with {tol} row(s)")
+           
+            if drop_exp:    
+                logging.info(f"Exception rows have been Dropped")
+        
+        except Exception as e:
+            raise FlightException(e,sys) from e
+    
+    def drop_duplicates(X : pd.DataFrame):
+        try:
+            if X.duplicated().sum():
+                X.drop_duplicates(ignore_index=True,inplace=True)
+                logging.info(f"Duplicate data has been removed from DataFrame")
+        
+        except Exception as e:
+            raise FlightException(e,sys) from e
     
     def split_data_as_train_test(self) -> DataIngestionArtifact:
         try:
             raw_data_dir = self.data_ingestion_config.raw_data_dir
             file_name = os.listdir(raw_data_dir)[0]
             # get the path to .csv file
-            housing_file_path = os.path.join(raw_data_dir,file_name)
+            flight_file_path = os.path.join(raw_data_dir,file_name)
 
-            logging.info(f"Reading csv file: [{housing_file_path}]")
-            housing_data_frame = pd.read_csv(housing_file_path)
+            logging.info(f"Reading csv file: [{flight_file_path}]")
+            flight_data_frame = pd.read_csv(flight_file_path)
 
-            # Stratify split by "median_income" column
-            housing_data_frame["income_cat"] = pd.cut(
-                housing_data_frame["median_income"],
-                bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
-                labels=[1,2,3,4,5]
+            # drop rows with only one category count in any columnm
+            self.drop_rows_with_exception(X=flight_data_frame,)
+            # drop Duplicate data
+            self.drop_duplicates(X=flight_data_frame)
+            
+            # Stratify split 
+            
+            min_price = flight_data_frame["Price"].describe("min")
+            q_one_price = flight_data_frame["Price"].describe("25%")
+            q_two_price = flight_data_frame["Price"].describe("50%")
+            q_three_price = flight_data_frame["Price"].describe("75%")
+            
+            flight_data_frame["Price_cat"] = pd.cut(
+                flight_data_frame["Price"],
+                bins=[min_price-1, q_one_price, q_two_price, q_three_price, np.inf],
+                labels=[1,2,3,4]
             )
             
+            
             logging.info(f"Splitting data into train and test")
+            strat_train_set = None
+            strat_test_set = None
 
-            split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, 
-                    random_state=42)
+            split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 
-            for train_index,test_index in split.split(housing_data_frame, housing_data_frame["income_cat"]):
-                strat_train_set = housing_data_frame.loc[train_index].drop([
-                    "income_cat"],axis=1)
-                strat_test_set = housing_data_frame.loc[test_index].drop([
-                    "income_cat"],axis=1)
+            for train_index,test_index in split.split(flight_data_frame, flight_data_frame["Price_cat"]):
+                strat_train_set = flight_data_frame.loc[train_index].drop(["Price_cat"],axis=1)
+                strat_test_set = flight_data_frame.loc[test_index].drop(["Price_cat"],axis=1)
             
             # get the file_paths for train & test
-            train_file_path = os.path.join(
-                self.data_ingestion_config.ingested_train_dir,file_name)
-            test_file_path = os.path.join(
-                self.data_ingestion_config.ingested_test_dir,file_name)
+            train_file_path = os.path.join(self.data_ingestion_config.ingested_train_dir,file_name)
+            test_file_path = os.path.join(self.data_ingestion_config.ingested_test_dir,file_name)
             
             # Export train & test to their respective folders
             if strat_train_set is not None:
                 os.makedirs(self.data_ingestion_config.ingested_train_dir,
                     exist_ok=True)
-                logging.info(f"Exporting training datset to file: ["
-                    "{train_file_path}]")
+                logging.info(f"Exporting training datset to file: [{train_file_path}]")
                 strat_train_set.to_csv(train_file_path,index=False)
 
             if strat_test_set is not None:
-                os.makedirs(self.data_ingestion_config.ingested_test_dir, 
-                    exist_ok= True)
+                os.makedirs(self.data_ingestion_config.ingested_test_dir, exist_ok= True)
                 logging.info(f"Exporting test dataset to file: [{test_file_path}]")
                 strat_test_set.to_csv(test_file_path,index=False)
             
@@ -136,7 +168,7 @@ class DataIngestion:
             return data_ingestion_artifact
             
         except Exception as e:
-            raise HousingException(e,sys) from e
+            raise FlightException(e,sys) from e
     
     def initiate_data_ingestion(self)-> DataIngestionArtifact:
         try:
@@ -148,7 +180,7 @@ class DataIngestion:
             return data_ingestion_artifact
         
         except Exception as e:
-            raise HousingException(e,sys) from e
+            raise FlightException(e,sys) from e
         
     def __del__(self):
         logging.info(f"{'='*20}Data Ingestion log completed.{'='*20} \n\n")
