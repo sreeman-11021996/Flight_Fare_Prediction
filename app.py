36d34bace3bc9a54c50ef4bf128ee41eb2bdff88
sreeman <sreemanbitsmech@gmail.com>
2022-07-28 21:15:59 +0530

    batch_predict.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 import sys
 
 import pip
@@ -13,18 +13,21 @@ from flight.constant import CONFIG_DIR, get_current_time_stamp
 from flight.pipeline.pipeline import Pipeline
 from flight.entity.flight_predictor import FlightPredictor, FlightData
 from flask import send_file, abort, render_template
+from werkzeug.utils import secure_filename
 
 
 ROOT_DIR = os.getcwd()
 LOG_FOLDER_NAME = "logs"
 PIPELINE_FOLDER_NAME = "flight"
 SAVED_MODELS_DIR_NAME = "saved_models"
+STATIC_KEY = "static"
+TEST_DATASET_DIR_NAME = "test_prediction"
 
 MODEL_CONFIG_FILE_PATH = os.path.join(ROOT_DIR, CONFIG_DIR, "model.yaml")
 LOG_DIR = os.path.join(ROOT_DIR, LOG_FOLDER_NAME)
 PIPELINE_DIR = os.path.join(ROOT_DIR, PIPELINE_FOLDER_NAME)
 MODEL_DIR = os.path.join(ROOT_DIR, SAVED_MODELS_DIR_NAME)
-
+TEST_DATASET_DIR = os.path.join(ROOT_DIR,STATIC_KEY,TEST_DATASET_DIR_NAME)
 
 FLIGHT_DATA_KEY = "flight_data"
 FLIGHT_PRICE_KEY = "Flight Price"
@@ -103,16 +106,31 @@ def predict():
 
 @app.route('/batch_predict', methods=['GET', 'POST'])
 def batch_predict():
-    context = None
+    context = {
+        FLIGHT_DATA_KEY: None,
+        FLIGHT_PRICE_KEY: None
+    }
     if request.method == "POST":
-        test_file = request.files['Upload File']
-        print(test_file)
+        context = {
+            FLIGHT_DATA_KEY: True,
+            FLIGHT_PRICE_KEY : 0
+        }
+        test_file = request.files['file']
+        if test_file.filename == '':
+            print("File name is invalid")
+            return redirect(request.url)
+        filename = secure_filename(test_file.filename)
+        time_stamp = get_current_time_stamp()
+        test_file_dir = os.path.join(TEST_DATASET_DIR,time_stamp)
+        os.makedirs(test_file_dir,exist_ok=True)
+        test_file.save(os.path.join(test_file_dir,filename))
+        
         return render_template("batch_predict.html",context=context)
     return render_template("batch_predict.html", context=context)
-
 
 
 if __name__ == "__main__":
     app.run(port=5000)
     
-    
+# <h5 class="card-title">Download File </h5>
+# <a class="btn btn-success" target="_blank" href="https://github.com/sreeman-11021996/Flight_Fare_Prediction/blob/main/dataset/test.csv">Download prediction</a>
